23dd433afd7bbca5d7cd9b3b071fff5db8910d85
Im-suhyeon <00suhyen@gmail.com>
2023-05-28 01:40:49 +0900

    230528 수현 swing root

---
+++ src/kiosk.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+
+public class kiosk {
+    private JPanel BasePanel;
+    private JPanel NorthPanel;
+    private JPanel SouthPanel;
+    private JPanel RightPanel;
+    private JPanel LeftPanel;
+    private JTable Cart;
+    private JButton OrderCancel;
+    private JButton OrderComplete;
+    private JButton PayCard;
+    private JPanel deriPanel;
+    private JPanel shrimpPanel;
+    private JPanel bulgogiPanel;
+    private JPanel cokePanel;
+    private JPanel icecreamPanel;
+    private JComboBox bulgogiAmount;
+    private JButton bulgogiCart;
+    private JPanel bulgogiImage;
+    private JComboBox shrimpAmount;
+    private JButton shrimpCart;
+    private JComboBox deriAmount;
+    private JButton deriCart;
+    private JComboBox cokeAmount;
+    private JButton cokeCart;
+    private JLabel Title;
+    private JComboBox icecreamAmount;
+    private JButton icecreamCart;
+    private JPanel deriImage;
+    private JLabel deriLabel;
+    private JLabel deriPrice;
+    private JLabel bulgogiLabel;
+    private JLabel bulgogiPrice;
+    private JLabel shrimpLabel;
+    private JLabel shrimpPrice;
+    private JPanel shrimpImage;
+    private JPanel cokeImage;
+    private JLabel cokeLabel;
+    private JLabel cokePrice;
+    private JLabel icecreamLabel;
+    private JPanel icecreamImage;
+    private JLabel icecreamPrice;
+    private JComboBox spriteAmount;
+    private JButton spriteCart;
+    private JPanel spritePanel;
+    private JLabel spriteLabel;
+    private JPanel spriteImage;
+    private JLabel spritePrice;
+
+    public kiosk() {
+    }
+
+    private void createUIComponents() {
+
+
+        // TODO: place custom component creation code here
+    }
+}
